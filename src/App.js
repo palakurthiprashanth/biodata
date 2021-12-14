db6d102e357db6fa7c4cb155d8f9aea7b07f8dd9
palakurthiprashanth <palakurthi.prashanth1@gmail.com>
2021-12-14 13:12:10 +0530

    Particles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import Navbar from './components/Navbar/Navbar';
@@ -7,6 +9,27 @@ import Header from './components/Header/Header';
 export default function App() {
   return (
     <div>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: '#f9ab00',
+              },
+            },
+          },
+        }}
+      />
       <Navbar />
       <Header />
     </div>
